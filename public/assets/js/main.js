d6cb7a4ad505098934ebb4cd6198fdc70361c6df
GitTher <estherfernandezflorez@gmail.com>
2021-12-31 10:30:24 +0100

    add global variables

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,13 @@
 'use strict';
 
+/* global variables */
+
+const animeInput = document.querySelector('.js-input');
+const submitBtn = document.querySelector('.js-submit');
+const resetBtn = document.querySelector('.js-reset');
+const favList = document.querySelector('.js-favourites');
+const resultList = document.querySelector('.js-results');
+
 console.log('>> Ready :)');
 
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
+/* global variables */
+
+const animeInput = document.querySelector('.js-input');
+const submitBtn = document.querySelector('.js-submit');
+const resetBtn = document.querySelector('.js-reset');
+const favList = document.querySelector('.js-favourites');
+const resultList = document.querySelector('.js-results');
+
 console.log('>> Ready :)');
