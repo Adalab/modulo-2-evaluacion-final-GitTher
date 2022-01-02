451715f8ebe8b04c6f29142a235bcea44be5cb0c
GitTher <estherfernandezflorez@gmail.com>
2022-01-02 17:33:41 +0100

    change renderResultList to use DOM instead of InnerHTML

--- src/js/01_fetch.js
+++ src/js/01_fetch.js
@@ -14,32 +14,65 @@ function renderResultList(resultElements) {
     resultList.innerHTML = `<h2 class="results__title">Resultados:</h2>`;
     for (let i = 0; i < resultElements.length; i++) {
         if (resultElements[i].image_url === 'https://cdn.myanimelist.net/images/qm_50.gif?s=e1ff92a46db617cb83bfc1e205aff620') {
-            resultList.innerHTML +=
-                ` 
-                <article class="results__card js-card">
-                <img
-                    src="https://via.placeholder.com/210x295/000000/ffffff/?text=${resultElements[i].title}"
-                    class="results__card--img"
-                    alt="${resultElements[i].title}"
-                />
-                <h3 class="results__card--title js-cardTitle">${resultElements[i].title}</h3>
-                </article>
-                `
-                ;
+            const resultCard = document.createElement('li');
+            const resultCardImg = document.createElement('img');
+            const resultCardTitle = document.createElement('h3');
+            const resultCardTitleContent = document.createTextNode(resultElements[i].title);
+
+            resultCard.appendChild(resultCardImg);
+            resultCard.appendChild(resultCardTitle);
+            resultCard.classList.add('results__card', 'js-card');
+
+            resultCardImg.src = `https://via.placeholder.com/210x295/000000/ffffff/?text=${resultElements[i].title}`;
+            resultCardImg.alt = resultElements[i].title;
+            resultCardImg.classList.add('results__card--img');
+
+            resultCardTitle.appendChild(resultCardTitleContent);
+            resultCardTitle.classList.add('results__card--title', 'js-cardTitle');
+
+            resultList.appendChild(resultCard);
+
+
+            // resultList.innerHTML +=
+            //     ` 
+            //     <article class="results__card js-card">
+            //     <img
+            //         src="https://via.placeholder.com/210x295/000000/ffffff/?text=${resultElements[i].title}"
+            //         class="results__card--img"
+            //         alt="${resultElements[i].title}"
+            //     />
+            //     <h3 class="results__card--title js-cardTitle">${resultElements[i].title}</h3>
+            //     </article>
+            //     `
+            //     ;
 
         } else {
-            resultList.innerHTML +=
-                ` 
-                <article class="results__card js-card">
-                <img
-                    src="${resultElements[i].image_url}"
-                    class="results__card--img"
-                    alt="${resultElements[i].title}"
-                />
-                <h3 class="results__card--title js-cardTitle">${resultElements[i].title}</h3>
-                </article>
-                `
-                ;
+            const resultCard = document.createElement('li');
+            const resultCardImg = document.createElement('img');
+            const resultCardTitle = document.createElement('h3');
+            const resultCardTitleContent = document.createTextNode(resultElements[i].title);
+            resultCard.appendChild(resultCardImg);
+            resultCard.appendChild(resultCardTitle);
+            resultCardTitle.appendChild(resultCardTitleContent);
+            resultCardImg.src = resultElements[i].image_url;
+            resultCardImg.alt = resultElements[i].title;
+            resultCardImg.classList.add('results__card--img');
+            resultCardTitle.classList.add('results__card--title', 'js-cardTitle');
+            resultCard.classList.add('results__card', 'js-card');
+            resultList.appendChild(resultCard);
+
+            // resultList.innerHTML +=
+            // ` 
+            // <article class="results__card js-card">
+            // <img
+            //     src="${resultElements[i].image_url}"
+            //     class="results__card--img"
+            //     alt="${resultElements[i].title}"
+            // />
+            // <h3 class="results__card--title js-cardTitle">${resultElements[i].title}</h3>
+            // </article>
+            // `
+            // ;
         }
         const animeCard = document.querySelectorAll('.js-card');
         for (const eachCard of animeCard) {
