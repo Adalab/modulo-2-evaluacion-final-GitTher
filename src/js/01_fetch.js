a127da81ea86056e99cbbb73abedfd83a8638668
GitTher <estherfernandezflorez@gmail.com>
2022-01-03 13:40:20 +0100

    clean renderResultList function

--- src/js/01_fetch.js
+++ src/js/01_fetch.js
@@ -12,67 +12,36 @@ function getAnime(event) {
 function renderResultList(resultElements) {
     resultList.innerHTML = '';
     for (let i = 0; i < resultElements.length; i++) {
+        const resultCard = document.createElement('li');
+        const resultCardImg = document.createElement('img');
+        const resultCardTitle = document.createElement('h3');
+        const resultCardTitleContent = document.createTextNode(resultElements[i].title);
+
+        resultCard.appendChild(resultCardImg);
+        resultCard.appendChild(resultCardTitle);
+        resultCard.classList.add('results__card', 'js-card');
+
+        resultCardImg.alt = resultElements[i].title;
+        resultCardImg.classList.add('results__card--img');
+
+        resultCardTitle.appendChild(resultCardTitleContent);
+        resultCardTitle.classList.add('results__card--title', 'js-cardTitle');
+
+        resultList.appendChild(resultCard);
+
         if (resultElements[i].image_url === 'https://cdn.myanimelist.net/images/qm_50.gif?s=e1ff92a46db617cb83bfc1e205aff620') {
-            const resultCard = document.createElement('li');
-            const resultCardImg = document.createElement('img');
-            const resultCardTitle = document.createElement('h3');
-            const resultCardTitleContent = document.createTextNode(resultElements[i].title);
-
-            resultCard.appendChild(resultCardImg);
-            resultCard.appendChild(resultCardTitle);
-            resultCard.classList.add('results__card', 'js-card');
-
             resultCardImg.src = `https://via.placeholder.com/210x295/bbe1fa/0f4c75/?text=${resultElements[i].title}`;
-            resultCardImg.alt = resultElements[i].title;
-            resultCardImg.classList.add('results__card--img');
-
-            resultCardTitle.appendChild(resultCardTitleContent);
-            resultCardTitle.classList.add('results__card--title', 'js-cardTitle');
-
-            resultList.appendChild(resultCard);
-
         } else if (resultElements[i].image_url === '') {
-            const resultCard = document.createElement('li');
-            const resultCardImg = document.createElement('img');
-            const resultCardTitle = document.createElement('h3');
-            const resultCardTitleContent = document.createTextNode(resultElements[i].title);
-
-            resultCard.appendChild(resultCardImg);
-            resultCard.appendChild(resultCardTitle);
-            resultCard.classList.add('results__card', 'js-card');
-
             resultCardImg.src = `https://via.placeholder.com/210x295/bbe1fa/0f4c75/?text=${resultElements[i].title}`;
-            resultCardImg.alt = resultElements[i].title;
-            resultCardImg.classList.add('results__card--img');
-
-            resultCardTitle.appendChild(resultCardTitleContent);
-            resultCardTitle.classList.add('results__card--title', 'js-cardTitle');
-
-            resultList.appendChild(resultCard);
         } else {
-            const resultCard = document.createElement('li');
-            const resultCardImg = document.createElement('img');
-            const resultCardTitle = document.createElement('h3');
-            const resultCardTitleContent = document.createTextNode(resultElements[i].title);
-
-            resultCard.appendChild(resultCardImg);
-            resultCard.appendChild(resultCardTitle);
-            resultCard.classList.add('results__card', 'js-card');
-
             resultCardImg.src = resultElements[i].image_url;
-            resultCardImg.alt = resultElements[i].title;
-            resultCardImg.classList.add('results__card--img');
-
-            resultCardTitle.appendChild(resultCardTitleContent);
-            resultCardTitle.classList.add('results__card--title', 'js-cardTitle');
-
-            resultList.appendChild(resultCard);
-
         }
+
         const animeCard = document.querySelectorAll('.js-card');
         for (const eachCard of animeCard) {
             eachCard.addEventListener('click', addFavourite);
         }
+
         compareLists();
 
     }
